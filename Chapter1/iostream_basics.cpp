9e29aaab80f75ba28cf6d3c460acdb758f8e2dba
tobias <tobias@dsaid.gov.sg>
2020-07-06 02:51:36 +0800

    Add exercises

---
+++ Chapter1/iostream_basics.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i{}; // initialised
+    cout << "Please enter a number: ";
+    cin >> i;
+    cout << "Your number is: " << i << '\n'; // note the single quote for literal
+}
---
+++ uninitialised_variables.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int x;
+
+    // you could get any random values because 
+    // it's based on what's at the memory address assigned
+    std::cout << x << std::endl;
+}
